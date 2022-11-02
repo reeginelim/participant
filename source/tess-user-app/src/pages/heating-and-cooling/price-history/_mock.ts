de55850b75e74c7688c35a09782c2c2bb8ca80a4
Du Huang <duhuang@andrew.cmu.edu>
2022-11-02 02:42:18 -0700

    Update Heating-and-Cooling Price History Page Add mock data and enable mock service.

---
+++ source/tess-user-app/src/pages/heating-and-cooling/price-history/_mock.ts
@@ -0,0 +1,26 @@
+import moment from 'moment';
+import type { Request, Response } from 'express';
+import type { PriceHistoryData } from './data.d';
+
+const priceHistoryLineChartData = [];
+for (let i = 0; i < 20; i += 1) {
+  const date = moment(new Date().getTime() + 1000 * 60 * 30 * i).format('HH:mm');
+  priceHistoryLineChartData.push({
+    date,
+    type: 'Price',
+    value: Math.floor(Math.random() * 100) + 10,
+  });
+}
+const getFakePriceHistoryData: PriceHistoryData = {
+  priceHistoryLineChartData,
+};
+
+const fakeChartData = (_: Request, res: Response) => {
+  return res.json({
+    data: getFakePriceHistoryData,
+  });
+};
+
+export default {
+  'GET  /api/fake-price-history-data': fakeChartData,
+};
---
+++ source/tess-user-app/src/pages/heating-and-cooling/price-history/index.tsx
@@ -0,0 +1,51 @@
+import { useRequest } from 'umi';
+import { Card, Statistic, Row, Col } from 'antd';
+import { Line } from '@ant-design/charts';
+import type { DataItem } from './data.d';
+import { fakeChartData } from './service';
+const PriceHistory = ({ priceHistoryLineChartData }: { priceHistoryLineChartData: DataItem[] }) => {
+  const { loading, data } = useRequest(fakeChartData);
+  console.log('data', data);
+  return (
+    <>
+      <Card>
+        <Row gutter={16}>
+          <Col span={6}>
+            <Statistic title="Yesterday's Ave Price" value={0.5} />
+          </Col>
+          <Col span={6}>
+            <Statistic title="Yesterday's Lowest Price" value={0.4} />
+          </Col>
+          <Col span={6}>
+            <Statistic title="Yesterday's Highest Price" value={0.65} />
+          </Col>
+        </Row>
+      </Card>
+      <Card>
+        <div style={{ padding: '0 24px' }}>
+          <Line
+            label={'test'}
+            forceFit
+            height={400}
+            data={data?.priceHistoryLineChartData || []}
+            responsive
+            xField="date"
+            yField="value"
+            seriesField="type"
+            interactions={[
+              {
+                type: 'slider',
+                cfg: {},
+              },
+            ]}
+            legend={{
+              position: 'top-center',
+            }}
+          />
+        </div>
+      </Card>
+    </>
+  );
+};
+
+export default PriceHistory;
