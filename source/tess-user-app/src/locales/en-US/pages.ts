22582de90fbbdfc15fcbc9e69db6ae33094fdc96
Du Huang <duhuang@andrew.cmu.edu>
2022-12-04 22:27:48 -0800

    Remove i18n.

--- source/tess-user-app/src/locales/en-US/pages.ts
+++ source/tess-user-app/src/locales/en-US/pages.ts
@@ -10,6 +10,7 @@ export default {
   'pages.login.password.placeholder': 'Password: ant.design',
   'pages.login.password.required': 'Please input your password!',
   'pages.login.phoneLogin.tab': 'Phone Login',
+  'pages.login.register.tab': 'Register',
   'pages.login.phoneLogin.errorMessage': 'Verification Code Error',
   'pages.login.phoneNumber.placeholder': 'Phone Number',
   'pages.login.phoneNumber.required': 'Please input your phone number!',
