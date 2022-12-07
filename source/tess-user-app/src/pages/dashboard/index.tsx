9e2aabd0e3b2e1218118ff938ff8d915a8abfb80
Du Huang <duhuang@andrew.cmu.edu>
2022-12-07 14:21:56 -0800

    Update data fetch.

--- source/tess-user-app/src/pages/dashboard/index.tsx
+++ source/tess-user-app/src/pages/dashboard/index.tsx
@@ -1,60 +1,20 @@
 import { Space, Table, Tag } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Amplify, API, graphqlOperation } from 'aws-amplify';
 import awsconfig from '../.././aws-exports';
 Amplify.configure(awsconfig);
 import * as queries from '../../graphql/queries';
 import * as mutations from '../../graphql/mutations';
 import { create } from 'lodash';
+import { query } from 'express';
 
-
-console.log('prev11 todos!!!!!');
-const id = "112233";
-//const todos = await API.graphql(graphqlOperation(queries.getTestDataModel,{ id: '34567' }));
-// const todos = await API.graphql({
-//   query: queries.getHcData,
-//   variables: { id: id }
-// });
-
-
-const schedule_obj = {
-  comfort : 50,
-  min_temp : 70,
-  max_temp : 75,
-  start_time: 1670198503,
-  end_time: 1670276068,
-  type: "HOME"
-}
-
-const hcSchedData = {
-  id: "112233",
-  device_id: "44556677",
-  schedule: schedule_obj
-};
-
-//await API.graphql(graphqlOperation(mutations.createHcSchedule, {input: hcSchedData}));
-
-//const todos = await API.graphql(graphqlOperation(queries.getTestDataModel,{ id: '34567' }));
-
-// console.log('postdatatoda details!!!!!', todoDetails);
-// console.log('postdata!!!!!', postdata);
-
-const hcschedule = await API.graphql({
-  query: queries.getHcSchedule,
-  variables: { id: id }
-});
-
-
-
-
-console.log('todos!!!!!', hcschedule.data.getHcSchedule.id);
-console.log('todos0001!!!!!', hcschedule);
+const id = '112233';
 interface DataType {
   key: string;
   name: string;
-  status: string; 
-  load: number
+  status: string;
+  load: number;
   cost: string;
 }
 
@@ -63,7 +23,7 @@ const columns: ColumnsType<DataType> = [
     title: 'Device Type',
     dataIndex: 'name',
     key: 'name',
-    render: text => <a>{text}</a>,
+    render: (text) => <a>{text}</a>,
   },
   {
     title: 'Status',
@@ -82,52 +42,80 @@ const columns: ColumnsType<DataType> = [
   },
 ];
 
-const data: DataType[] = [
-  {
-    key: '1',
-    name: 'Heating & Cooling',
-    status: 'On',
-    load: 100, 
-    cost: '80/hr'
+// const dataSource: DataType[] = [
+//   {
+//     name: 'Heating & Cooling',
+//     status: 'On',
+//     load: 100,
+//     cost: '80/hr',
+//   },
+//   {
+//     name: 'Water Heater',
+//     status: 'On',
+//     load: 100,
+//     cost: '80/hr',
+//   },
+//   {
+//     name: 'Solar Panels',
+//     status: 'On',
+//     load: 100,
+//     cost: '80/hr',
+//   },
+//   {
+//     name: 'Electric Vehicle',
+//     status: 'On',
+//     load: 100,
+//     cost: '80/hr',
+//   },
+//   {
+//     name: 'Electric Vehicle Charger',
+//     status: 'On',
+//     load: 100,
+//     cost: '80/hr',
+//   },
+//   {
+//     name: 'Battery Storage',
+//     status: 'On',
+//     load: 100,
+//     cost: '80/hr',
+//   },
+// ];
 
-  },
-  {
-    key: '2',
-    name: 'Water Heater',
-    status: 'On',
-    load: 100, 
-    cost: '80/hr'
-  },
-  {
-    key: '3',
-    name: 'Solar Panels',
-    status: 'On',
-    load: 100, 
-    cost: '80/hr'
-  },
-  {
-    key: '4',
-    name: 'Electric Vehicle',
-    status: 'On',
-    load: 100, 
-    cost: '80/hr'
-  },
-  {
-    key: '5',
-    name: 'Electric Vehicle Charger',
-    status: 'On',
-    load: 100, 
-    cost: '80/hr'
-  },
-  {
-    key: '6',
-    name: 'Battery Storage',
-    status: 'On',
-    load: 100, 
-    cost: '80/hr'
-  },
-];
-
-const App: React.FC = () => <Table columns={columns} dataSource={data} />;
+const App: React.FC = () => {
+  const [data, setData] = useState(); // data should be an array
+  const [loading, setLoading] = useState(false);
+  const convert = (input) => {
+    // Convert fetched data format to table
+    return [
+      {
+        key: '1',
+        name: input.data.getHcSchedule.id,
+        status: 'On',
+        load: input.data.getHcSchedule.schedule.comfort,
+        cost: '100/hr',
+      },
+    ];
+  };
+  const fetchData = () => {
+    // Fetch data from database
+    setLoading(true);
+    const hcSchedulePromise = API.graphql({
+      query: queries.getHcSchedule,
+      variables: { id: id },
+    });
+    hcSchedulePromise.then((res) => {
+      const converted = convert(res);
+      console.log(converted);
+      setData(converted);
+      setLoading(false);
+    });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <Table rowKey={(record) => record.name} loading={loading} columns={columns} dataSource={data} />
+  );
+};
 
 export default App;
