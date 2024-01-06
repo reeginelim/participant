e248c8d02916e8f9b59362ed6db1e6538a703067
David P. Chassin <david.chassin@me.com>
2024-01-06 15:24:29 -0800

    Update testdata

--- data/testdata.py
+++ data/testdata.py
@@ -11,7 +11,7 @@ questions = pd.read_csv("questions.csv",index_col="Question")
 FILE = open("answers_2.csv","wt")
 header = 'Timestamp,"' + '","'.join(questions.index) + '"'
 print(header,file=FILE)
-for n in range(10000):
+for n in range(1000):
     row = [str(DATE)]
     for question,answers in questions.iterrows():
         if question == "What state do you live in?":
